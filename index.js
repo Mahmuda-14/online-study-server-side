753c00cd3afa0870279e5c5dfdeb1c2db74221a1
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2023-11-09 14:38:39 +0600

    modified

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const verifyToken = async(req, res, next) =>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const assCollection = client.db('assDB').collection('task');
 
